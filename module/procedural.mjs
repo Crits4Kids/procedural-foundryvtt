b46c93556751e7c10b5fef6283251b9bf31f04b5
Matt Kniller <mkniller@twistedtristudios.com>
2026-08-05 22:51:25 -0400

    feat: add system entry point registering data models and document class

---
+++ module/procedural.mjs
@@ -0,0 +1,24 @@
+import TropeActorData from "./data/actor-trope.mjs";
+import NpcActorData from "./data/actor-npc.mjs";
+import TropeItemData from "./data/item-trope.mjs";
+import TalentItemData from "./data/item-talent.mjs";
+import EquipmentItemData from "./data/item-equipment.mjs";
+import ProceduralActor from "./documents/actor.mjs";
+import { registerChatListeners } from "./helpers/chat-listeners.mjs";
+import { seedCompendiums } from "./helpers/seed-compendiums.mjs";
+
+Hooks.once("init", () => {
+  CONFIG.Actor.dataModels.trope = TropeActorData;
+  CONFIG.Actor.dataModels.npc = NpcActorData;
+  CONFIG.Item.dataModels.trope = TropeItemData;
+  CONFIG.Item.dataModels.talent = TalentItemData;
+  CONFIG.Item.dataModels.equipment = EquipmentItemData;
+
+  CONFIG.Actor.documentClass = ProceduralActor;
+
+  registerChatListeners();
+});
+
+Hooks.once("ready", () => {
+  seedCompendiums();
+});
